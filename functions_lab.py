21447e2c6c1d8ae06da25f86b4f14f74a7afd723
Daniel Slebodnick <DanSlebodnick@Garys-MBP.cable.rcn.com>
2022-07-01 08:36:50 -0400

    wip: still working on solutions

---
+++ functions_lab.py
@@ -0,0 +1,24 @@
+# Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n. For example:
+
+def sum_to(n):
+  n+-1
+  return 1 + n
+
+
+# Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+def largest(*args):
+  list = [1, 2, 3, 4, 5]
+  return
+# Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
+
+def occurances(hello, l):
+  return
+
+
+# Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product.
+# (HINT: Review your notes on *args).
+
+def product(*args):
+  return 
+
